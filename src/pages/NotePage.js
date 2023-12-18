7c1a9f47879579fee872152cfc8276654761ccc5
Terence Jiang <terencejiang2004@gmail.com>
2023-12-17 22:53:17 -0500

    first upload

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -16,9 +16,20 @@ const NotePage = () => {
   }, [id])
 
   let getNote = async async =>{
+    if (id === 'new') return
     let response = await fetch(`http://localhost:8000/notes/${id}`)
     let data = await response.json()
     setNote(data)
+  }
+
+  let createNote = async () => {
+    await fetch(`http://localhost:8000/notes/`, {
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({...note, 'updated': new Date()})
+    })
   }
 
   let updateNote = async () => {
@@ -46,8 +57,10 @@ const NotePage = () => {
 
     if(id !== 'new' && !note.body){
       deleteNote()
-    }else if(id === 'new'){
-      updateNote
+    }else if(id !== 'new'){
+      updateNote()
+    }else if(id === 'new' && note.body != null){
+      createNote()
     }
 
     updateNote()
@@ -63,7 +76,12 @@ const NotePage = () => {
           </Link>
         </h3>
 
-        <butto onClick={deleteNote}>Delete</butto>
+        {id !== 'new' ? (
+          <button onClick={deleteNote}>Delete</button>
+        ): (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+        
       </div>
         <textarea onChange={(e)=> {setNote({...note, 'body':e.target.value})}} value={note?.body}>
 
